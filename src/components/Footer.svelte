fb048bb78a8c3fb3b7021b7f656b2bc466640cc5
Amine AML <amin_aml@live.fr>
2021-10-27 22:31:34 +0100

    chore: add an RSS link to the header and add a footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,11 +2,37 @@
     
 </script>
 
-<footer>
-    <ul>
-        <li>RSS</li>
-        <li>Sitemap</li>
-    </ul>
-    Copyright @ Amine Amellouk
-    Made with love and built with Astro - Source code available on GitHub
+<footer class="flex">
+    <div class="mx-auto space-y-1 flex flex-col">
+        <ul class="flex space-x-2 justify-center underline">
+            <li class="text-blue-700">
+                <a href="/rss.xml" class="flex space-x-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                    </svg>
+                    <span>RSS</span>
+                </a>
+            </li>
+            <li>|</li>
+            <li class="text-blue-700">
+                <a href="/sitemap.xml" class="flex space-x-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                    </svg>
+                    <span>Sitemap</span>
+                </a>
+            </li>
+        </ul>
+        <span class="flex justify-center">Copyright &copy; 2020, Amine Amellouk</span>
+        <span class="flex justify-center space-x-1">
+            <span>Made with</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="red">
+                <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+            </svg>
+            <span>and built with</span>
+            <a href="https://astro.build/" class="text-blue-700">Astro</a>
+            <span>- Source code available on</span>
+            <a href="https://github.com/AmineAML/blog-astro-svelte" class="text-blue-700">GitHub</a>
+        </span>
+    </div>
 </footer>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -7,6 +7,7 @@
         <div class="items-stretch hidden lg:flex">
             <a class="btn btn-ghost btn-sm rounded-btn" href="/" aria-label="blog"> Blog </a>
             <a class="btn btn-ghost btn-sm rounded-btn" href="/about" aria-label="about"> About </a>
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/rss.xml" aria-label="about"> RSS </a>
         </div>
     </div>
 </header>
