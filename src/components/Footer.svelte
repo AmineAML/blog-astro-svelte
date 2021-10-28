92e0f346b9ad4dcf5a8e41af34e274b35fe9c625
Amine AML <amin_aml@live.fr>
2021-10-28 18:16:18 +0100

    chore: use site from env var for astro config, change name and add description and author name for package file, fix footer in the bottom of pages, create and add a thumbnail for the blog, remove unnessary code line, change the about author, refactor posts component and the component using it, update styles for header and footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -24,7 +24,7 @@
             </li>
         </ul>
         <span class="flex justify-center">Copyright &copy; 2020, Amine Amellouk</span>
-        <span class="flex justify-center space-x-1">
+        <span class="flex flex-wrap space-x-1 justify-center">
             <span>Made with</span>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="red">
                 <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -4,7 +4,7 @@
 
 <header class="navbar mb-2 shadow-lg rounded-box">
     <div class="flex-1 px-2 mx-2">
-        <div class="items-stretch hidden lg:flex">
+        <div class="items-stretch lg:flex">
             <a class="btn btn-ghost btn-sm rounded-btn" href="/" aria-label="blog"> Blog </a>
             <a class="btn btn-ghost btn-sm rounded-btn" href="/about" aria-label="about"> About </a>
             <a class="btn btn-ghost btn-sm rounded-btn" href="/rss.xml" aria-label="about"> RSS </a>
